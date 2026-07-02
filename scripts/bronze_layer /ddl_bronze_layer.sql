664df16a744d4034902f3c4867db32179f7dba88
Sha^-^tha 🦅 <Shathah2030@gmail.com>
2026-07-02 03:00:15 +0300

    Define DDL for CRM and ERP tables in bronze layer
    Added DDL for multiple CRM and ERP tables in the bronze layer, including customer, product, sales details, and location tables.

--- scripts/bronze_layer /ddl_bronze_layer.sql
+++ scripts/bronze_layer /ddl_bronze_layer.sql
@@ -9,17 +9,13 @@ Script Purpose:
 	  Run this script to re-define the DDL structure of 'bronze_layer' Tables
 ===============================================================================
 */
-
-
---Create Tables (CRM) ======================================================================
-
-
+--Create Tables (CRM) 
+--crm_cust_info
+--Check if the table exists in the bronze layer, and drop it before recreating to avoid duplication errors
 IF OBJECT_ID('bronze_layer.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE  bronze_layer.crm_cust_info;
 GO
-
 CREATE TABLE bronze_layer.crm_cust_info(
-
 	cst_id             INT,
 	cst_key            NVARCHAR(50) ,
 	cst_firstname      NVARCHAR(50),
@@ -27,19 +23,14 @@ CREATE TABLE bronze_layer.crm_cust_info(
 	cst_marital_status NVARCHAR(50),
 	cst_gndr           NVARCHAR(50),
 	cst_create_date    DATE
-
 );
 
 GO
-
----------------------------------------------------------------------
-  
+--crm_prd_info
 IF OBJECT_ID('bronze_layer.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE bronze_layer.crm_prd_info;
 GO
-
 CREATE TABLE bronze_layer.crm_prd_info(
-
 	prd_id       INT,
 	prd_key      NVARCHAR(50) ,
 	prd_nm       NVARCHAR(50),
@@ -47,19 +38,13 @@ CREATE TABLE bronze_layer.crm_prd_info(
 	prd_line     NVARCHAR(50),
 	prd_start_dt DATETIME,
 	prd_end_dt   DATETIME
-
 );
-
 GO
-
-
----------------------------------------------------------------------
+--crm_sales_details
 IF OBJECT_ID('bronze_layer.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze_layer.crm_sales_details ;
 GO
-
 CREATE TABLE bronze_layer.crm_sales_details(
-
 	sls_ord_num	 NVARCHAR(50),
 	sls_prd_key	 NVARCHAR(50) ,
 	sls_cust_id  INT,
@@ -69,54 +54,36 @@ CREATE TABLE bronze_layer.crm_sales_details(
 	sls_sales    INT,
 	sls_quantity INT,
 	sls_price    INT
-
 );
-
 GO
-
---Create Tables (ERP) ======================================================================
-
+--Create Tables (ERP)
+--erp_loc_a101
 IF OBJECT_ID('bronze_layer.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE bronze_layer.erp_loc_a101;
 GO
-
-
 CREATE TABLE bronze_layer.erp_loc_a101 (
-
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50)
-
 );
 GO
-
-
----------------------------------------------------------------------
+--erp_cust_az12
 IF OBJECT_ID('bronze_layer.erp_cust_az12', 'U') IS NOT NULL
     DROP TABLE bronze_layer.erp_cust_az12;
 GO
-
 CREATE TABLE bronze_layer.erp_cust_az12 (
-
     cid    NVARCHAR(50),
     bdate  DATE,
     gen    NVARCHAR(50)
-
 );
-
 GO
-
----------------------------------------------------------------------
+--erp_px_cat_g1v2
 IF OBJECT_ID('bronze_layer.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE bronze_layer.erp_px_cat_g1v2;
 GO
-
 CREATE TABLE bronze_layer.erp_px_cat_g1v2 (
-
     id           NVARCHAR(50),
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
     maintenance  NVARCHAR(50)
-
 );
-
 GO
